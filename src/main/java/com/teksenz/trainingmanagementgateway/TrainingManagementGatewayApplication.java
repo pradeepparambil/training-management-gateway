190851998594c1ad32ec7c4e8f387e7e942cf003
pradeep.pp <pradeep.pp@gmail.com>
2020-08-15 17:01:05 -0400

    Initial add

---
+++ src/main/java/com/teksenz/trainingmanagementgateway/TrainingManagementGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.teksenz.trainingmanagementgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingManagementGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingManagementGatewayApplication.class, args);
+	}
+
+}
